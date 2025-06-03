fb14bf4905d7ed9c650799164e810636150beef3
kuceraf06 <133848829+kuceraf06@users.noreply.github.com>
2025-06-03 10:40:53 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -17,14 +17,27 @@ require_once '../skeleton/auth.php';
 <?php include '../skeleton/headerAdmin.php'?>
     <div class="content">
         <div class="addBox">
-            <a href="/admin/udalosti/pridat/" class="childBox">
-                <i class='bx bx-plus'></i>
-                <p>Přidat novou událost</p>
-            </a>
-            <a href="/admin/program/pridat/" class="childBox">
-                <i class='bx bx-plus'></i>
-                <p>Přidat nový program</p>
-            </a>
+            <div class="firstBox">
+                <a href="/admin/udalosti/pridat/" class="childBox">
+                    <i class='bx bx-plus'></i>
+                    <p>Přidat novou událost</p>
+                </a>
+                <a href="/admin/program/pridat/" class="childBox childBox-2">
+                    <i class='bx bx-plus'></i>
+                    <p>Přidat nový program</p>
+                </a>
+            </div>
+            <div class="secondBox">
+                <a href="/admin/program/pridat/" class="childBox">
+                    <i class='bx bx-plus'></i>
+                    <p>Aktualizovat termín</p>
+                </a>
+                <a href="/admin/program/pridat/" class="childBox childBox-2">
+                    <i class='bx bx-plus'></i>
+                    <p>Přidat novou pozvánku</p>
+                </a>
+            </div>
         </div>
     </div>
+</body>
 </html>
