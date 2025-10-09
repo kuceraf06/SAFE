396324443f034108a5cb39cb6216d00b8e7f6041
kuceraf06 <133848829+kuceraf06@users.noreply.github.com>
2025-10-09 16:19:01 +0200

    Add files via upload

--- onas/index.php
+++ onas/index.php
@@ -1,3 +1,34 @@
+<?php
+include '../skeleton/db_connect.php';
+
+$sql = "SELECT datum_cas FROM termin LIMIT 1";
+$result = $conn->query($sql);
+
+$termin = null;
+$row = $result->fetch(PDO::FETCH_ASSOC);
+if ($row) {
+    $termin = $row['datum_cas'];
+}
+
+function cz_month($monthNumber) {
+    $months = [
+        1 => 'ledna',
+        2 => 'února',
+        3 => 'března',
+        4 => 'dubna',
+        5 => 'května',
+        6 => 'června',
+        7 => 'července',
+        8 => 'srpna',
+        9 => 'září',
+        10 => 'října',
+        11 => 'listopadu',
+        12 => 'prosince'
+    ];
+    return $months[$monthNumber] ?? '';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="cs">
     <head>
@@ -28,7 +59,7 @@
                         <p>SAFE je tradiční slavnostní galavečer baseballového a softballového oddílu <a href="https://www.minerskladno.cz" target="_blank" class="bolder">Miners Kladno</a>, který uzavírá uplynulou soutěžní sezonu a završuje celoroční snažení všech hráčů a trenérů klubu.
                         Na tomto slavnostním zakončení sezóny zhodnotíme každoroční výkony, odměníme úspěchy a pogratulujeme těm nejlepším. Již tradičně na tuto akci zveme vzácné hosty z řad našich zřizovatelů a podporovatelů klubu jako představitele Města Kladna a Středočeského kraje a dále baseballové a softballové reprezentanty a reprezentantky.
                         SAFE je také poděkováním našim <a href="../sponzori/" class="site-links">sponzorům</a>, fanouškům, rodičům a celé naší Miners Family za nekončící podporu, bez které bychom to, co děláme, dělat nemohli. 
-                        Užijte si i letošní SAFE 24 jak náleží, těšíme se na Vás.<br>
+                        Užijte si i letošní SAFE jak náleží, těšíme se na Vás.<br>
                         Více o SAFE se můžete dozvědět <a href="#history" class="site-links">zde</a>.<br>
                         Kdy a kde se akce koná se dozvíte <a href="#location" class="site-links">níže</a>.</p>
                     </div>
@@ -43,9 +74,18 @@
                     <div class="about-content">
                         <h2 id="location">Kdy a kde?</h2>
                         <p>SAFE se tradičně pořádá vždy po konci sezóny.</p>
-                        <p><strong>KDY:</strong> 9. listopadu od 18:00</p>
+                        <p><strong>KDY:</strong><?php if ($termin):
+                                                      $datetime = new DateTime($termin);
+                                                      $day = $datetime->format('j');
+                                                      $month = cz_month((int)$datetime->format('n'));
+                                                      $year = $datetime->format('Y');
+                                                      $time = $datetime->format('H:i'); ?>
+                                                      <?php echo "$day. $month $year od $time"; ?>
+                                                    <?php else: ?>
+                                                        <p>Termín nebyl nastaven.</p>
+                                                    <?php endif; ?></p>
                         <p><strong>ADRESA:</strong> <a target="_blank" href="https://www.google.com/maps/place//data=!4m2!3m1!1s0x470bb7da602a6c4b:0xe7204d94c85ab6b1?sa=X&ved=1t:8290&ictx=111">T. G. Masaryka 2320, 272 01 Kladno 1</a></p>
-                        <p><strong>KUDY:</strong> Kino Sokol se nachází v Kladně u nám. Svobody na pěší zóně. Lehce se k nám dostanete mětskou 
+                        <p><strong>KUDY:</strong> Kino Sokol se nachází v Kladně u nám. Svobody na pěší zóně. Lehce se k nám dostanete městskou 
                                                   či pražskou dopravou autobusy (všechny spoje staví na náměstí)</p>
                     </div>
                 </section>
