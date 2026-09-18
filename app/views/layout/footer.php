b410cfd80324e174a5eb02c582e8935d283ee150
Filip Kučera <133848829+kuceraf06@users.noreply.github.com>
2026-09-18 11:00:35 +0200

    Add files via upload

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -34,7 +34,7 @@ $foot = [
                             <h4><?= $foot['follow'] ?></h4>
                             <div class="social-links">
                                 <a href="https://www.facebook.com/minerskladno" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                                <a href="https://www.flickr.com/photos/201375961@N07/" target="_blank"><i class='bx bxl-flickr'></i></a>
+                                <a href="https://www.flickr.com/photos/201375961@N07/" target="_blank" aria-label="Zonerama"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor" aria-hidden="true" focusable="false" style="height:1em;width:.875em;vertical-align:-.125em"><path d="M52 0 L396 0 A52 52 0 0 1 448 52 L448 132 A34 34 0 0 1 438 156 L252 349 A10 10 0 0 0 260 366 L428 366 A20 20 0 0 1 448 386 L448 460 A52 52 0 0 1 396 512 L52 512 A52 52 0 0 1 0 460 L0 374 A20 20 0 0 1 6 360 L196 163 A10 10 0 0 0 188 146 L34 146 A34 34 0 0 1 0 112 L0 52 A52 52 0 0 1 52 0 Z"/></svg></a>
                                 <a href="https://www.instagram.com/minerskladno/" target="_blank"><i class="fab fa-instagram"></i></a>
                             </div>
                         </div>
